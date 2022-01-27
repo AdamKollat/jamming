a48ca513af741c146107d4b595c9dd1b7ac56b7d
Adam <adam.kollat@gmail.com>
2022-01-27 12:31:24 +0000

    Add spotify.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,9 +2,12 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
+
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
   constructor(props) {
@@ -51,7 +54,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({ searchResults: searchResults })
+    })
   }
 
   render() {
